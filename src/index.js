bb4b54997f0405ccd7492eef143a8a683d11ced9
Nekoya3 <mame3619@gmail.com>
2020-03-12 00:57:37 +0900

    GraphQL仮完成

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 const {GraphQLServer} = require('graphql-yoga');
-const data = require('./data.json')
+const contacts = require('./datas/contacts.json');
+const current_patients = require('./datas/current_patients.json');
+const discharges_summary = require('./datas/discharges_summary.json');
+const inspections = require('./datas/inspections.json');
+const last_update = require('./datas/last_update.json');
+const patients = require('./datas/patients.json');
+const patients_summary = require('./datas/patients_summary.json');
+const querents = require('./datas/querents.json');
 
 const resolvers = {
     Query: {
-        datas: (obj, args, context, info) => {
-            return data;
+        contacts: (obj, args, context, info) => {
+            return contacts;
         },
+        current_patients: (obj, args, context, info) => {
+            return current_patients;
+        },
+        discharges_summary: (obj, args, context, info) => {
+            return discharges_summary;
+        },
+        inspections: (obj, args, context, info) => {
+            return inspections;
+        },
+        last_update: (obj, args, context, info) => {
+            return last_update;
+        },
+        patients: (obj, args, context, info) => {
+            return patients;
+        },
+        patients_summary: (obj, args, context, info) => {
+            return patients_summary;
+        },
+        querents: (obj, args, context, info) => {
+            return querents;
+        },
+
     },
 };
 
